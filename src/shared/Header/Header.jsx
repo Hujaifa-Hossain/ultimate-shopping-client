2fd6314a0c9423ee10f4682620bb33d7076daca1
Hujaifa21 <21hujaifa@gmail.com>
2022-10-26 21:09:52 +0600

    sliding header top created

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,17 +1,40 @@
 import { Link } from 'react-router-dom';
+import Slider from "react-slick";
 import './Header.scss';
 
 const Header = () => {
+
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: "linear"
+  };
+
+  const offers = ['SALE: UP TO 50% OFF ON SELECTED STYLES', 'ENJOY FREE DELIVERY ON ALL ORDERS', 'GET APEX GIFT VOUCHER AT FLAT 25% OFF']
+
   return (
-    <header className='header'>
-      <div className="logo">
+    <div>
+
+
+      <Slider {...settings} className='header-top'>
+        {offers.map(offer => <p>{offer}</p>)}
+      </Slider>
+
+
+
+      {/* <div className="logo">
         <Link to='/'>LOGO</Link>
       </div>
       <ul className="menu">
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
-      </ul>
-    </header>
+      </ul> */}
+    </div>
   );
 };
 
