1d7df303db690538e4f4e1040227e6f5f9c5512d
Hujaifa21 <21hujaifa@gmail.com>
2022-10-27 23:54:09 +0600

    banner added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,75 +1,27 @@
-import React from 'react';
+import './Home.scss';
+import Slider from "react-slick";
 
 const Home = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
-    <div>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
-      <h2>Home </h2>
+    <div style={{width: '100%'}}>
+      <Slider {...settings} className='home'>
+          <div>
+            <img src="https://storage.sg.content-cdn.io/in-resources/25c7d1c6-73be-4ff9-b000-0bf110b5c461/Images/userimages/banners/oct2022/fashion_week_hm_d2210.jpg" alt="" />
+          </div>
+          <div>
+            <img src="https://storage.sg.content-cdn.io/in-resources/25c7d1c6-73be-4ff9-b000-0bf110b5c461/Images/userimages/banners/sept2022/sustainable_shoes_hmbanr_d0809.jpg" alt="" />
+          </div>
+          <div>
+            <img src="https://storage.sg.content-cdn.io/in-resources/25c7d1c6-73be-4ff9-b000-0bf110b5c461/Images/userimages/banners/oct2022/home_banr_03_d2010.jpg" alt="" />
+          </div>
+        </Slider>
     </div>
   );
 };
